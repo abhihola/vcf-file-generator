4931a0467d1a6a2d5c71830545a8fdb60631adda
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-09 10:30:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public')); // Serve frontend files
+app.use(express.static(__dirname + '/public')); // Serve frontend files
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
