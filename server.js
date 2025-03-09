8d9806c1f594bbd3f310c6b6d30fe14563f3b381
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-09 10:36:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const nodeCron = require('node-cron');
 const vcfGenerator = require('./vcf_generator');
+const path = require('path');
 
 dotenv.config();
 const app = express();
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public')); // Serve frontend files
+app.use(express.static('public')); // Serve frontend files
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -35,9 +36,14 @@ app.post('/submit', async (req, res) => {
     }
 });
 
-// Serve the Home Page
+// Serve the main page (form submission)
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Serve the home page for downloading VCF file
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 // Schedule VCF generation every 3 days
