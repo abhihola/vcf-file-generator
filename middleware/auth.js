35908b0b5cfc7c9ce1782ffae9253e8e2ccb94a1
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-10 20:08:39 +0100

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+    next();
+  } else {
+    res.status(403).json({ error: "Unauthorized access" });
+  }
+};
